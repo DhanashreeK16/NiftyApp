42f90973b88f2425142f6afce1a4bcba9cd35b97
Dhanashree Kshirsagar <dhanashreekshirsagar16@gmail.com>
2026-02-07 14:35:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown("""
 # 3. Load Data
 @st.cache_data
 def load_data():
-    df = pd.read_csv("../yfinance_Project/Stock_NS.csv")
+    df = pd.read_csv("Stock_NS.csv")
     df['Date'] = pd.to_datetime(df['Date'])
     return df
 
